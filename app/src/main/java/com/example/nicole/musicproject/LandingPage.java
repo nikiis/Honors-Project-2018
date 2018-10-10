b9c52c7efd25eca0a6b64c9b8e00ee7313bbcac3
Nicole Shek <nicoleshek315@gmail.com>
2018-10-10 21:56:47 +0100

    started designing homepage, can now play chord c through music file but needs refined

---
+++ app/src/main/java/com/example/nicole/musicproject/LandingPage.java
@@ -0,0 +1,20 @@
+package com.example.nicole.musicproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+
+    public void onClickPlay(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/nicole/musicproject/MainActivity.java
+++ app/src/main/java/com/example/nicole/musicproject/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.nicole.musicproject;
 
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer cChord = MediaPlayer.create(this, R.raw.cchord);
+        Button playC = this.findViewById(R.id.playCButton);
+        playC.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                cChord.start();
+            }
+        });
     }
 }
+
+
+
+
+
+
+
